1e5b92cad457a3d3927a4ef8d8adf0e3a7a06ab0
Nishil <nishilkapadia17@gmail.com>
2021-01-05 00:29:35 +0530

    :sparkles: First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## DevConnecter 
+- MERN Stack Front To Back: Full Stack React, Redux & Node.js from BRAD 
+
+Technologies
+- JWT
+- Postman
+- Mongoose/ Atlas /Mongo DB
+- Bcrypt Password Hashing
+- Heroku and Git Deployment
+- React Hooks
+- Redux
---
+++ server.js
@@ -0,0 +1,13 @@
+// Imports
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('API Online!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log('App listening on port 3000!');
+});
