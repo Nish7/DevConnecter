bb14b78d71844c87320868e28013f7dbdc3877d3
Nishil <nishilkapadia17@gmail.com>
2021-01-12 14:06:50 +0530

    :fire: Prod. Ready

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Connect Database
 connectDB();
@@ -8,15 +9,20 @@ connectDB();
 // Init Middleware for Body Parser
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-	res.send('API Online!');
-});
-
 // Mounted Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve Static Assets in Production
+if (process.env.NODE_ENV === 'production') {
+	// Set the static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 // Create the Server
 const PORT = process.env.PORT || 5000;
